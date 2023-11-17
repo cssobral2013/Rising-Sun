04ff05a178da8dfc0d483a39689e18147dbde168
Cristiano Sobral <cssobral2013@gmail.com>
2023-11-17 00:10:49 -0300

    Update v4.320

---
+++ generate-woff-woff2.py
@@ -0,0 +1,333 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/RisingSun-Thin.ttf'
+woffPath = './web/RisingSun-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/RisingSun-ThinItalic.ttf'
+woffPath = './web/RisingSun-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/RisingSun-ExtraLight.ttf'
+woffPath = './web/RisingSun-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/RisingSun-ExtraLightItalic.ttf'
+woffPath = './web/RisingSun-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/RisingSun-Light.ttf'
+woffPath = './web/RisingSun-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-LightItalic.ttf'
+woffPath = './web/RisingSun-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-LightItalic.ttf'
+woffPath = './web/RisingSun-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-Regular.ttf'
+woffPath = './web/RisingSun-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-Italic.ttf'
+woffPath = './web/RisingSun-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-Medium.ttf'
+woffPath = './web/RisingSun-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-MediumItalic.ttf'
+woffPath = './web/RisingSun-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-SemiBold.ttf'
+woffPath = './web/RisingSun-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-SemiBoldItalic.ttf'
+woffPath = './web/RisingSun-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-SemiBold.ttf'
+woffPath = './web/RisingSun-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-SemiBoldItalic.ttf'
+woffPath = './web/RisingSun-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-Bold.ttf'
+woffPath = './web/RisingSun-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-BoldItalic.ttf'
+woffPath = './web/RisingSun-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-ExtraBold.ttf'
+woffPath = './web/RisingSun-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-ExtraBoldItalic.ttf'
+woffPath = './web/RisingSun-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-Black.ttf'
+woffPath = './web/RisingSun-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/RisingSun-BlackItalic.ttf'
+woffPath = './web/RisingSun-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/RisingSun-Thin.ttf'
+woff2Path = './web/RisingSun-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/RisingSun-ThinItalic.ttf'
+woff2Path = './web/RisingSun-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/RisingSun-ExtraLight.ttf'
+woff2Path = './web/RisingSun-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/RisingSun-ExtraLightItalic.ttf'
+woff2Path = './web/RisingSun-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/RisingSun-Light.ttf'
+woff2Path = './web/RisingSun-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-LightItalic.ttf'
+woff2Path = './web/RisingSun-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-LightItalic.ttf'
+woff2Path = './web/RisingSun-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-Regular.ttf'
+woff2Path = './web/RisingSun-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-Italic.ttf'
+woff2Path = './web/RisingSun-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-Medium.ttf'
+woff2Path = './web/RisingSun-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-MediumItalic.ttf'
+woff2Path = './web/RisingSun-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-SemiBold.ttf'
+woff2Path = './web/RisingSun-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-SemiBoldItalic.ttf'
+woff2Path = './web/RisingSun-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-SemiBold.ttf'
+woff2Path = './web/RisingSun-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-SemiBoldItalic.ttf'
+woff2Path = './web/RisingSun-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-Bold.ttf'
+woff2Path = './web/RisingSun-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-BoldItalic.ttf'
+woff2Path = './web/RisingSun-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-ExtraBold.ttf'
+woff2Path = './web/RisingSun-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-ExtraBoldItalic.ttf'
+woff2Path = './web/RisingSun-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-Black.ttf'
+woff2Path = './web/RisingSun-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/RisingSun-BlackItalic.ttf'
+woff2Path = './web/RisingSun-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
